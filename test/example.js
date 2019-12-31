84fedb83d3adafb4716a1ac692c2129a837f495e
Arcane Bot Center List <contact@arcane-botcenter.xyz>
2019-12-31 01:15:57 +0100

    Update example.js

--- test/example.js
+++ test/example.js
@@ -6,6 +6,6 @@ const { Client } = require('discord.js'),
 client.login('Bot TOKEN');
 
 client.on('ready',() =>{
-        abcAPI.login('API Key', client.user.id);
+    abcAPI.login('API Key', client.user.id);
     abcAPI.post(client.guilds.size, client.users.size, "Number of shards");
 });
