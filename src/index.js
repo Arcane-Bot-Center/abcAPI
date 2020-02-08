4b746d11156aa74d3a435520c4b03de3e8d4249e
Arcane Bot Center List <contact@arcane-botcenter.xyz>
2020-02-08 20:03:22 +0100

    Update index.js
    Added a new feature: update

--- src/index.js
+++ src/index.js
@@ -1,71 +1,57 @@
-const { post } = require('axios');
-
 let settings = {
     token : '',
     id :''
 };
 
-module.exports = {
-    login :(token, id) => new Promise((resolve, reject) => {
-        if (typeof token != 'string') {
-            reject(new Error('[Arcane-wrapper] No token provided'));
-        }
+module.exports ={
+    login: (token, id) => new Promise((resolve, reject) => {
+
+        if (typeof token != 'string') { reject(new Error('[Arcane-wrapper] No token provided')); }
+        if(isNaN(id)) { reject(new Error('[Arcane-wrapper] No id provided')); }
 
         settings.token = token;
-
-        if(isNaN(id)){
-            reject(new Error('[Arcane-wrapper] No id provided'));
-        }
-
         settings.id = id;
+
         resolve('[Arcane-wrapper] Ready to post');
     }),
 
-    post: (server, user, shard) => new Promise((resolve, reject) => {
-        if (!settings.token) {
-            reject(new Error('[Arcane-wrapper] No token provided'));
-        }
-
-        if (!settings.id) {
-            reject(new Error('[Arcane-wrapper] No id provided'));
-        }
-
-        if (server) {
-            if (isNaN(server)) {
-                reject(new TypeError('[Arcane-wrapper] Please indicate a valid number (server) !'));
-            }
-        } else if (user) {
-            if (isNaN(user)) {
-                reject(new TypeError('[Arcane-wrapper] Please indicate a valid number (user) !'));
-            }
-        } else if (shard) {
-            if (isNaN(user)) {
-                reject(new TypeError('[Arcane-wrapper] Please indicate a valid number (shard) !'));
-            }
-        }
+    post: (client) => new Promise((resolve, reject) => {
+        if(!client) { reject(new Error('[Arcane-wrapper] No library provided')); }
+        if (!settings.token) { reject(new Error('[Arcane-wrapper] No token provided, You must initialized the module')); }
+        if (!settings.id) { reject(new Error('[Arcane-wrapper] No id provided, You must initialized the module')); }
 
         let send = {
-            member_count : user ? user : 0,
-            server_count : server ? server : 0,
-            shard_count: shard ? shard : 0,
+            member_count : client.users.size ? client.users.size : 0,
+            server_count : client.guilds.size ? client.guilds.size : 0,
+            shard_count: client.shard ? client.shard.count : 0,
         };
 
         console.log('[Arcane-wrapper] Send Data in progress...');
         sendData(send);
-    })
+    }),
+
+    update: (client) => {
+        setInterval(function () {
+            require('./index.js').post(client)
+        }, 60000);
+    }
 };
 
 function sendData(send) {
     const content = JSON.stringify(send, null);
-    post(`https://arcane-botcenter.xyz/api/${settings.id}/stats`, content, {
+    require('axios').post(`https://arcane-botcenter.xyz/api/${settings.id}/stats`, content, {
         headers: {
             'Content-Type': 'application/json',
             'Content-Length': content.length,
             'Authorization': settings.token
         }
     }).then((res) => {
-        console.log(`[Arcane-wrapper] Stats posted ! => https://arcane-botcenter.xyz/api/${settings.id}/stats \n\nGuild: ${send.server_count}\nUsers: ${send.member_count}\nShard: ${send.shard_count}`);
-    }).catch((err)=> {
-        console.error(`[Arcane-wrapper] Stats post error ${err}`);
+        if (res.status === 200){
+            console.log(`[Arcane-wrapper] Stats posted ! => https://arcane-botcenter.xyz/api/${settings.id}/stats \n\nGuild: ${send.server_count}\nUsers: ${send.member_count}\nShard: ${send.shard_count}`);
+        } else {
+            console.log('An error has occurred');
+        }
+    }).catch((err) => {
+        console.log(`[Arcane-wrapper] Stats post error ${err}`);
     });
 }
