1c8874bd207e603ad450431cb0b6d2fa3ff8bf81
Arcane Bot Center List <contact@arcane-botcenter.xyz>
2020-02-08 15:32:39 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,55 +5,55 @@ let settings = {
     id :''
 };
 
-module.exports.login = (token, id) => new Promise((resolve, reject) => {
-
-    if (typeof token != 'string') {
-        reject(new Error('[Arcane-wrapper] No token provided'));
-    }
-
-    settings.token = token;
-
-    if(isNaN(id)){
-        reject(new Error('[Arcane-wrapper] No id provided'));
-    }
-
-    settings.id = id;
-    resolve('[Arcane-wrapper] Ready to post');
-});
-
-module.exports.post = (server, user, shard) => new Promise((resolve, reject) => {
-
-    if (!settings.token) {
-        reject(new Error('[Arcane-wrapper] No token provided'));
-    }
-
-    if (!settings.id) {
-        reject(new Error('[Arcane-wrapper] No id provided'));
-    }
-
-    if (server) {
-        if (isNaN(server)) {
-            reject(new TypeError('[Arcane-wrapper] Please indicate a valid number (server) !'));
+module.exports = {
+    login :(token, id) => new Promise((resolve, reject) => {
+        if (typeof token != 'string') {
+            reject(new Error('[Arcane-wrapper] No token provided'));
         }
-    } else if (user) {
-        if (isNaN(user)) {
-            reject(new TypeError('[Arcane-wrapper] Please indicate a valid number (user) !'));
-        }
-    } else if (shard) {
-        if (isNaN(user)) {
-            reject(new TypeError('[Arcane-wrapper] Please indicate a valid number (shard) !'));
-        }
-    }
 
-    let send = {
-        member_count : user ? user : 0,
-        server_count : server ? server : 0,
-        shard_count: shard ? shard : 0,
-    };
+        settings.token = token;
 
-    console.log('[Arcane-wrapper] Send Data in progress...');
-    sendData(send);
-});
+        if(isNaN(id)){
+            reject(new Error('[Arcane-wrapper] No id provided'));
+        }
+
+        settings.id = id;
+        resolve('[Arcane-wrapper] Ready to post');
+    }),
+
+    post: (server, user, shard) => new Promise((resolve, reject) => {
+        if (!settings.token) {
+            reject(new Error('[Arcane-wrapper] No token provided'));
+        }
+
+        if (!settings.id) {
+            reject(new Error('[Arcane-wrapper] No id provided'));
+        }
+
+        if (server) {
+            if (isNaN(server)) {
+                reject(new TypeError('[Arcane-wrapper] Please indicate a valid number (server) !'));
+            }
+        } else if (user) {
+            if (isNaN(user)) {
+                reject(new TypeError('[Arcane-wrapper] Please indicate a valid number (user) !'));
+            }
+        } else if (shard) {
+            if (isNaN(user)) {
+                reject(new TypeError('[Arcane-wrapper] Please indicate a valid number (shard) !'));
+            }
+        }
+
+        let send = {
+            member_count : user ? user : 0,
+            server_count : server ? server : 0,
+            shard_count: shard ? shard : 0,
+        };
+
+        console.log('[Arcane-wrapper] Send Data in progress...');
+        sendData(send);
+    })
+};
 
 function sendData(send) {
     const content = JSON.stringify(send, null);
