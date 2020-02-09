d403ea4b6a9689894ba74ead63c18d02de2d8b7d
Arcane Bot Center List <contact@arcane-botcenter.xyz>
2020-02-09 15:14:17 +0100

    updates every 10 minutes

--- test/example.js
+++ test/example.js
@@ -7,6 +7,6 @@ client.login("Bot TOKEN");
 
 client.on('ready',() =>{
     abcAPI.login(AbcToken, client.user.id); // => require to use this module, log in to abcAPI
-    abcAPI.update(client); // => made update every minutes
+    abcAPI.update(client); // => updates every 10 minutes
     abcAPI.post(client); // => made simple post to this abcAPI
 });
