e5c141b45faf8c5e386ce1139e8440097e42526a
Arcane Bot Center List <contact@arcane-botcenter.xyz>
2020-02-09 14:26:18 +0100

    [UPDATE] Multiple shards support

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ let settings = {
     id :''
 };
 
-module.exports ={
+module.exports = {
     login: (token, id) => new Promise((resolve, reject) => {
 
         if (typeof token != 'string') { reject(new Error('[Arcane-wrapper] No token provided')); }
-        if(isNaN(id)) { reject(new Error('[Arcane-wrapper] No id provided')); }
+        if(isNaN(id)) { reject(new Error('[Arcane-wrapper] No id provided'));  }
 
         settings.token = token;
         settings.id = id;
@@ -15,25 +15,31 @@ module.exports ={
         resolve('[Arcane-wrapper] Ready to post');
     }),
 
-    post: (client) => new Promise((resolve, reject) => {
+    post: (client) => new Promise(async (resolve, reject) => {
+        let users;
+        let guilds;
+        
         if(!client) { reject(new Error('[Arcane-wrapper] No library provided')); }
         if (!settings.token) { reject(new Error('[Arcane-wrapper] No token provided, You must initialized the module')); }
         if (!settings.id) { reject(new Error('[Arcane-wrapper] No id provided, You must initialized the module')); }
-
+        if(client.shard) {
+            users = await client.shard.fetchClientValues('users.size'),
+            guilds = await client.shard.fetchClientValues('guilds.size');
+        }
         let send = {
-            member_count : client.users.size ? client.users.size : 0,
-            server_count : client.guilds.size ? client.guilds.size : 0,
-            shard_count: client.shard ? client.shard.count : 0,
+            member_count : client.shard ? users.reduce((prev, val) => prev + val, 0) : client.users.size ? client.users.size : 0,
+            server_count : client.shard ? guilds.reduce((prev, val) => prev + val, 0) : client.guilds.size ? client.guilds.size : 0,
+            shard_count : client.shard ? client.shard.count : 0,
         };
 
         console.log('[Arcane-wrapper] Send Data in progress...');
         sendData(send);
     }),
-
+    
     update: (client) => {
         setInterval(function () {
             require('./index.js').post(client)
-        }, 60000);
+        }, 60000)
     }
 };
 
@@ -49,7 +55,7 @@ function sendData(send) {
         if (res.status === 200){
             console.log(`[Arcane-wrapper] Stats posted ! => https://arcane-botcenter.xyz/api/${settings.id}/stats \n\nGuild: ${send.server_count}\nUsers: ${send.member_count}\nShard: ${send.shard_count}`);
         } else {
-            console.log('An error has occurred');
+            console.log('An error has occurred')
         }
     }).catch((err) => {
         console.log(`[Arcane-wrapper] Stats post error ${err}`);
