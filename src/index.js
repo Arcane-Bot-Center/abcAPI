24037b9c5aeb357f477b05c0fa5348a92e6e0563
Androz2091 <androz2091@gmail.com>
2020-02-12 09:46:38 +0100

    Add support for master djs branch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+const useDjsMaster = false;
+try {
+    const DjsVersion = require('discord.js').version;
+    if(DjsVersion.split('.').shift() === '12') useDjsMaster = true;
+} catch(e) {
+    useDjsMaster = false;
+}
+
 let settings = {
     token : '',
     id :''
@@ -23,12 +31,12 @@ module.exports = {
         if (!settings.token) { reject(new Error('[Arcane-wrapper] No token provided, You must initialized the module')); }
         if (!settings.id) { reject(new Error('[Arcane-wrapper] No id provided, You must initialized the module')); }
         if(client.shard) {
-            users = await client.shard.fetchClientValues('users.size'),
-            guilds = await client.shard.fetchClientValues('guilds.size');
+            users = await client.shard.fetchClientValues(useDjsMaster ? 'users.cache.size' : 'users.size'),
+            guilds = await client.shard.fetchClientValues(useDjsMaster ? 'guilds.cache.size' : 'users.size');
         }
         let send = {
-            member_count : client.shard ? users.reduce((prev, val) => prev + val, 0) : client.users.size ? client.users.size : 0,
-            server_count : client.shard ? guilds.reduce((prev, val) => prev + val, 0) : client.guilds.size ? client.guilds.size : 0,
+            member_count : client.shard ? users.reduce((prev, val) => prev + val, 0) : (useDjsMaster ? (client.users.cache.size ? client.users.cache.size : 0) : (client.users.size ? client.users.size : 0)),
+            server_count : client.shard ? guilds.reduce((prev, val) => prev + val, 0) : (useDjsMaster ? (client.guilds.cache.size ? client.guilds.cache.size : 0) : (client.guilds.size ? client.guilds.size : 0)),
             shard_count : client.shard ? client.shard.count : 0,
         };
 
