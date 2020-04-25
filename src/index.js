174af3708fce2eacee7ff3b6d5daa416cc763519
zechaos031 <delrieu.adrien@gmail.com>
2020-04-25 18:44:24 +0200

    [Addings] add discord.js exemple

--- src/index.js
+++ src/index.js
@@ -1,57 +1,142 @@
 const { EventEmitter } = require('events');
 const Logger = require('./utils/logger');
 class ABCapi extends EventEmitter {
-    constructor(opts = { lib, client, token }) {
+    constructor(opts = { lib, client, token,autoSend:true }) {
         super(opts);
         this.opts = opts;
         this.run = false;
         this.cooldown = 10000;
         setTimeout(() => { this._ready(); }, 2000);
-
-
     }
     _ready(){
         if(!this.run){
-            this.emit('ready');
             this.run = true;
-            setInterval(() => { this.update(); }, this.cooldown);
+            this.emit('ready');
+            this.opts.autoSend ? setInterval(() => { this.update(); }, this.cooldown): false;
         }else{
             Logger.log('Module already launched')
         }
     }
 
     update(){
+        console.log(this.run)
+        if(!this.run) Logger.error('Module not started')
         let lib = this.opts.lib.toString().toLowerCase();
         if(lib === 'eris'){
             require('./lib/eris').run(this.opts).then(res => {
                 if(res.status === 200){
-                    this.emit('post')
+                    this.emit('post',res.data)
                 }
             }).catch((err)=>{
-                if(err.response.status === 429){
-                    this.emit('rateLimited')
+                if(err.response){
+                    if(err.response.status === 429){
+                        err.data = {
+                            message:'You have been ratelimited please contact the support'
+                        };
+                        this.emit('rateLimited',err.data)
+                    }else{
+                        this.emit('error',err)
+                    }
                 }else{
-                    this.emit('error')
+                    Logger.CriticalError(`A critical error has occurred`,'0x0000503',err)
                 }
+
             });
         }else if(lib === 'discord' || lib === 'discordjs' || lib === 'discord.js'){
             require('./lib/discordjs').run(this.opts).then(res => {
                 if(res.status === 200){
-                    this.emit('post')
+                    this.emit('post',res.data)
                 }
             }).catch((err)=>{
-                if(err.response.status === 429){
-                    this.emit('rateLimited',err.data)
+                if(err.response){
+                    if(err.response.status === 429){
+                        err.data = {
+                            message:'You have been ratelimited please contact the support'
+                        };
+                        this.emit('rateLimited',err.data)
+                    }else{
+                        console.log(err.data)
+                        this.emit('error',err)
+                    }
                 }else{
-                    this.emit('error',err)
+                    Logger.CriticalError(`A critical error has occurred`,'0x0000503',err)
+                }
+            })
+        }else if(lib === 'eris sharder' || lib === 'erissharder' || lib === 'eris-sharder'){
+            require('./lib/eris-sharder').run(this.opts).then(res => {
+                if(res.status === 200){
+                    this.emit('post',res.data)
+                }
+            }).catch((err)=>{
+                if(err.response){
+                    if(err.response.status === 429){
+                        err.data = {
+                            message:'You have been ratelimited please contact the support'
+                        };
+                        this.emit('rateLimited',err.data)
+                    }else{
+                        this.emit('error',err)
+                    }
+                }else{
+                    Logger.CriticalError(`A critical error has occurred`,'0x0000503',err)
                 }
             })
         }else{
-            Logger.CriticalError('Unknown library supported \'discord.js\', and \'eris\'.','0x0000404')
+            Logger.CriticalError('Unknown library supported \'discord.js\' , \'eris-sharder\', and \'eris\'.','0x0000404',`Library not found, you provided ${this.opts.lib}`)
         }
     }
 
+    getInfo(id) {
 
+        return new Promise((resolve, reject) => {
+            resolve('This endpoint is not implemented');
+            const {get} = require('axios');
+            get(`https://arcane-center.xyz/api/${id}/stats`).then((res) => {
+                let data = {
+                    status: res.data.status,
+                    content: {
+                        verified: res.data.verified,
+                        certified: res.data.certified,
+                        owner: res.data.owner,
+                        co_owner: res.data.co_owner,
+                        addedAt: res.data.timestamp,
+                        premium: res.data.premium,
+                        votes: res.data.voted.size,
+                        bot: {
+                            botid: res.data.bot._id,
+                            name: res.data.bot.username
+                        },
+                        infos: {
+                            tags: res.data.infos.tags,
+                            lib: res.data.infos.lib,
+                            prefix: res.data.infos.prefix,
+                            smallDesc: res.data.infos.sdesc,
+                            invite: res.data.infos.link,
+                            website: res.data.infos.website,
+                            github: res.data.infos.github,
+                            support: res.data.infos.support,
+                            nsfwWarn: res.data.infos.nsfwWarn,
+                            nsfwBlur: res.data.infos.nsfwBlur,
+                            youtube: res.data.infos.youtube
+                        },
+                        premium_infos: {
+                            paypal: res.data.premium_infos.paypal,
+                            tipeee: res.data.premium_infos.tipeee,
+                            patreon: res.data.premium_infos.patreon,
+                        },
+                        api: {
+                            vote: res.data.api.vote,
+                            shard: res.data.api.shard,
+                            server: res.data.api.server
+                        }
+                    }
+                };
+                resolve(data)
+            }).catch((err) => {
+                reject(err)
+            })
+        })
+    }
 
 }
 
---
+++ test/discordjs/main.js
@@ -0,0 +1,35 @@
+const {Client} = require("discord.js");
+const client = new Client();
+let api = require('../../main');
+
+client.login('BotToken');
+
+client.on('ready',()=>{
+    const  arcane = new api({ lib:'discord.js',
+        client:client, // Your client
+        token:'abcapi token', //your abc Api token
+        autoSend: true    // send automaticly stats (arcane.update() not require). Default is true
+    });
+
+    arcane.on("ready",() =>{
+        console.log('ABC API ready');
+        arcane.update() // made simple post require if autoSend is false
+    });
+
+    arcane.on('post',(data)=>{
+        console.log(data)
+        // return { server_count: Your guild number, shard_count:  Your shard number }
+    });
+    arcane.on('error',(error)=>{
+        console.log(error)
+        //return error
+    });
+    arcane.on('rateLimited',(responce)=>{
+        console.log(responce)
+        //return You have been ratelimited please contact the support
+    })
+
+    arcane.getInfo(botid).then((data)=>{
+        console.log(data)
+    })
+})
